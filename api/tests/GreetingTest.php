96dd4eef4ff4c8626411947c15160f666d2d27c7
Antonio Peric-Mazar <antonio@locastic.com>
2019-09-03 20:54:31 +0200

    added fixtures, tests

---
+++ api/tests/GreetingTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Tests;
+
+use App\Entity\Greeting;
+use Hautelook\AliceBundle\PhpUnit\RefreshDatabaseTrait;
+use Symfony\Bundle\FrameworkBundle\Client;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class ApiTest extends BaseTest
+{
+    /**
+     * Retrieves the book list.
+     */
+    public function testRetrieveTheBookList(): void
+    {
+        $response = $this->request('GET', '/greetings');
+        $json = json_decode($response->getContent(), true);
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals('application/ld+json; charset=utf-8', $response->headers->get('Content-Type'));
+
+        $this->assertArrayHasKey('hydra:totalItems', $json);
+        $this->assertEquals(11, $json['hydra:totalItems']);
+
+        $this->assertArrayHasKey('hydra:member', $json);
+        $this->assertCount(11, $json['hydra:member']);
+    }
+
+
+    /**
+     * Creates a greeting.
+     */
+    public function testCreateAGreeting(): void
+    {
+        $data = [
+            'name' => 'Persistence in PHP with Doctrine ORM',
+        ];
+
+        $response = $this->request('POST', '/greetings', $data);
+        $json = json_decode($response->getContent(), true);
+
+        $this->assertEquals(201, $response->getStatusCode());
+        $this->assertEquals('application/ld+json; charset=utf-8', $response->headers->get('Content-Type'));
+
+        $this->assertArrayHasKey('name', $json);
+        $this->assertEquals('Persistence in PHP with Doctrine ORM', $json['name']);
+    }
+
+    /**
+     * Updates a greeting.
+     */
+    public function testUpdateAGreeting(): void
+    {
+        $data = [
+            'name' => 'updated title',
+        ];
+
+        $response = $this->request('PUT', $this->findOneIriBy(Greeting::class, ['name' => 'test']), $data);
+        $json = json_decode($response->getContent(), true);
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals('application/ld+json; charset=utf-8', $response->headers->get('Content-Type'));
+
+        $this->assertArrayHasKey('name', $json);
+        $this->assertEquals('updated title', $json['name']);
+    }
+
+    /**
+     * Deletes a book.
+     */
+    public function testDeleteAGreeting(): void
+    {
+        $response = $this->request('DELETE', $this->findOneIriBy(Greeting::class, ['name' => 'test']));
+
+        $this->assertEquals(204, $response->getStatusCode());
+
+        $this->assertEmpty($response->getContent());
+    }
+
+    protected function findOneIriBy(string $resourceClass, array $criteria): string
+    {
+        $resource = static::$container->get('doctrine')->getRepository($resourceClass)->findOneBy($criteria);
+
+        return static::$container->get('api_platform.iri_converter')->getIriFromitem($resource);
+    }
+}
